8bc697ec047bf2b2939ebea39fbe06b843e3a523
RIX <69199129+SeunScape@users.noreply.github.com>
2023-09-12 21:35:37 +0100

    testing

--- src/app.js
+++ src/app.js
@@ -6,9 +6,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-// app.post('/api', (req, res) => {
-   
-//   });
+app.post("/api", (req, res) => {
+    const { name } = req.body;
+    
+    const insertQuery = "INSERT INTO persons (name) VALUES (?)";
+    db.query(insertQuery, [name], (err, result) => {
+        if (err) {
+          console.error("Error inserting name:", err);
+          res.status(500).json({ error: "Error inserting name" });
+          return;
+        }
+    
+        console.log("Name inserted successfully");
+        res.status(200).json({ message: "Name inserted successfully" });
+      });
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening on port ' + listener.address().port)
--- src/dbConnection.js
+++ src/dbConnection.js
@@ -17,21 +17,6 @@ connection.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-app.post("/api", (req, res) => {
-    const { name } = req.body;
-    
-    const insertQuery = "INSERT INTO persons (name) VALUES (?)";
-    db.query(insertQuery, [name], (err, result) => {
-        if (err) {
-          console.error("Error inserting name:", err);
-          res.status(500).json({ error: "Error inserting name" });
-          return;
-        }
-    
-        console.log("Name inserted successfully");
-        res.status(200).json({ message: "Name inserted successfully" });
-      });
-})
 connection.end(function(err) {
     if (err) {
       return console.log(err.message);
