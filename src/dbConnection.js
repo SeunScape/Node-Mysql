dd544f95625b4dcf5c5b3fdef8e53406cbf0318a
RIX <69199129+SeunScape@users.noreply.github.com>
2023-09-13 16:05:29 +0100

    .env changes

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 const mysql = require('mysql2');
-// const dotenv = require('dotenv');
-// dotenv.config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'myDbInstance',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME, 
 });
 
 connection.connect((err) => {
@@ -16,6 +15,8 @@ connection.connect((err) => {
   }
   console.log('Connected to MySQL database');
 });
+
+
 
 // connection.end(function(err) {
 //     if (err) {
